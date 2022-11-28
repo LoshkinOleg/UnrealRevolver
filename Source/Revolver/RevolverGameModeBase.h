ca4b13a74ba547e5bba5fdbc9a60eabbbe4e6be8
OlegLoshkin <loshkin.oleg.95@gmail.com>
2022-11-28 14:43:03 +0100

    first commit

---
+++ Source/Revolver/RevolverGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RevolverGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REVOLVER_API ARevolverGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
